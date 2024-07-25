692bcbf1a8a077fcc0f378ff87f47dfe86030a74
Jose Rios <joserios0531@gmail.com>
2024-07-25 16:11:10 -0700

    Fixed artist info card when no information is populated, made slight adjustments to the cards structure also

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const songInput = document.getElementById("song-entry");
-const lyrics = document.getElementById("lyrics");
-const btn = document.getElementById("confirmation");
-const artistPhoto = document.getElementById("artist-photo");
+// Headers
 const artistHeader = document.getElementById("artist-header");
 const albumHeader = document.getElementById("album-header");
 const songHeader = document.getElementById("song-header");
+
+// Entries
+const btn = document.getElementById("confirmation");
+const songInput = document.getElementById("song-entry");
+
+// Information Plugs
+const lyrics = document.getElementById("lyrics");
+const artistPhoto = document.getElementById("artist-photo");
 const art = document.getElementById("artist-content");
 const lyricsContent = document.getElementById("lyrics-content");
 const alerter = document.getElementById("alerter");
 const resultsContainer = document.getElementById("result-container");
 const album = document.getElementById("album-photo");
 const preview = document.getElementById("preview");
+const showArtistInfo = document.getElementById("artist-info-name");
 
-// Form Submission Efficiency
+// Removing Refresh on Form Submission
 btn.addEventListener('keypress', function(event){
     if(event.key === 'Enter'){
         event.preventDefault();
@@ -22,37 +28,50 @@ btn.addEventListener('keypress', function(event){
 
 // API (INFORMATION RETRIEVAL)
 btn.addEventListener('click', async (e) => {
-    //var song = songInput.value.trim();
-    var song = "ivy"
+    var song = songInput.value.trim();
+
+    resultsContainer.innerHTML = '';
+
+    if(song === ''){
+        alerter.classList.add("show");
+        setTimeout(function(){
+            alerter.classList.remove("show")
+        }, 3500)
+        return;
+    }
 
     await fetch(`https://api.lyrics.ovh/suggest/${song}&fmt=json`)
         .then(response => response.json())
         .then(data => {
-            let resultsLength = JSON.parse(data['data'].length);
-
-            if(resultsLength === 0){
-                console.log("No Results Found")
+            
+            if(data.data.length === 0){
+                var newResult = document.createElement('div')
+                newResult.classList.add("results-row")
+                newResult.classList.add("no-results-found")
+                newResult.innerHTML = "No Results Found"
+                resultsContainer.appendChild(newResult);
+                return
             }
 
             for(i=0; i< 5; i++){
                 var currentArtist = data.data[i].artist.name;
                 var currentSong = data.data[i].title;
                 var currentImg = data.data[i].artist.picture;
-                var adding = document.createElement('div');
+                var newResult = document.createElement('div');
 
-                adding.classList.add("result-row");
-                adding.innerHTML =`
-                    <div class="result-row-info">
-                        <img class="result-image" src="${currentImg}" alt="">
-                        <div class="artist-song-info">
-                            <div class="result-header">${currentArtist}</div>
-                            <div class="result-header">${currentSong}</div>
-                        </div>
+                newResult.classList.add("result-row");
+                newResult.innerHTML =
+                `<div class="result-row-info">
+                    <img class="result-image" src="${currentImg}" alt="">
+                    <div class="artist-song-info">
+                        <div class="result-header">${currentArtist}</div>
+                        <div class="result-header">${currentSong}</div>
                     </div>
-                    <div class="result-btn-container">
-                        <button class="find-lyrics-btn id=button-test">View Lyrics</button>
-                    </div>`
-                resultsContainer.appendChild(adding);
+                </div>
+                <div class="result-btn-container">
+                    <button class="find-lyrics-btn id=button-test">View Lyrics</button>
+                </div>`
+                resultsContainer.appendChild(newResult);
             }
 
             var buttons = document.querySelectorAll('.find-lyrics-btn');
@@ -61,7 +80,7 @@ btn.addEventListener('click', async (e) => {
                     populateLyrics(data, index)
                 });
             });
-            populateLyrics(data,0)
+            populateLyrics(data,0) // DEL
         })
 })
 
@@ -71,17 +90,17 @@ function populateLyrics(data, index){
     var tempAlbum = data.data[index].album.title;
     var artistPic = data.data[index].artist.picture;
     var albumPic = data.data[index].album.cover;
+
     preview.setAttribute('src', data.data[index].preview);
-    
     artistPhoto.setAttribute('src',`${artistPic}`);
     album.setAttribute('src', `${albumPic}`);
-
 
     fetch(`https://api.lyrics.ovh/v1/${tempName}/${tempSong}`)
     .then(response => response.json())
     .then(data => {
         if(data['lyrics'] === undefined){
-            lyrics.innerHTML = "Could not find song"
+            lyrics.innerHTML = "No Lyrics Found"
+            lyricsContent.classList.add("no-lyrics");
         }else{
             lyrics.innerHTML = data['lyrics']
         }
@@ -92,7 +111,8 @@ function populateLyrics(data, index){
 
         if(lyrics.textContent.trim() !== ''){
             lyricsContent.classList.add("show");
-            art.classList.add("show");
+            showArtistInfo.classList.add("show")
+            art.classList.add("show")
         }
     })    
 }
